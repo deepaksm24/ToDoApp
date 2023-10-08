6e106a38c1ca6c85734ab1f660aa08318ea23965
deepak <dpkmdu@gmail.com>
2023-10-08 21:31:44 +0530

    5

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
+// server.js
+
 const express = require('express');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
-require('dotenv').config();
-
-
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Render!12311213223');
+// Use the cors middleware with appropriate options
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+  credentials: true, // Enable cookies and authentication headers if needed
+}));
+
+// Define a sample API route
+app.get('/api/data', (req, res) => {
+  const data = { message: 'Hello from the Node.js API!' };
+  res.json(data);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  
-
 });
